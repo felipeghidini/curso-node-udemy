1b5f53ffaf888dbaf6a8ad78f66a3d0b277eea6d
felipeghidini <lipeghidini18@gmail.com>
2022-08-24 15:09:02 -0300

    Fundamentos

---
+++ arquivos/2_FUNDAMENTOS/2_import_export/index.mjs
@@ -0,0 +1,5 @@
+import soma from './meu_modulo.mjs';
+
+soma(10, 20);
+soma(102, 20);
+soma(10, 202);
---
+++ arquivos/2_FUNDAMENTOS/8_chalk/index.js
@@ -0,0 +1,9 @@
+const chalk = require("chalk");
+
+const nota = 6;
+
+// if (nota >= 7) {
+//     console.log(chalk.green('Parabéns você foi aprovado!'));
+// } else {
+//     console.log(chalk.bgRed.black('Você precisa fazer a prova novamente!'));
+// }

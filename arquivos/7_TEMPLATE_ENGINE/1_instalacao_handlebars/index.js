1b475c3c7495e98b4100bb81127648f1b18b2f6f
felipeghidini <lipeghidini18@gmail.com>
2022-11-10 07:12:32 -0300

    Handlebars - concluido

--- arquivos/7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
+++ arquivos/7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
@@ -3,8 +3,57 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', exphbs.engine());
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/dashboard', (req, res) => {
+
+    const items = ['Item a', 'Item b', 'Item c'];
+
+    res.render('dashboard', { items })
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender Node.js...',
+        comments: 4
+    }
+
+    res.render('blogpost', { post })
+})
+
+app.get('/blog', (req, res) => {
+    const posts = [
+        {
+            title: 'Aprender Node.js',
+            category: 'JavaScript',
+            body: 'Este artigo vai te ajudar a aprender Node.js...',
+            comments: 4
+        },
+        {
+            title: 'Aprender Java',
+            category: 'Java',
+            body: 'Este artigo vai te ajudar a aprender Node.js...',
+            comments: 4
+        },
+        {
+            title: 'Aprender TypeScript',
+            category: 'TypeScript',
+            body: 'Este artigo vai te ajudar a aprender Node.js...',
+            comments: 4
+        }
+    ]
+
+    res.render('blog', { posts })
+})
 
 app.get('/', (req, res) => {
 
@@ -14,7 +63,13 @@ app.get('/', (req, res) => {
         age: 33
     }
 
-    res.render('home', { data: user })
+    const palavra = 'Teste';
+
+    const auth = true;
+
+    const approved = false;
+
+    res.render('home', { data: user, palavra, auth, approved })
 })
 
 app.listen(3000, () => {
